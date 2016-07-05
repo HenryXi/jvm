59d5e86fd29003b6deaa4d4399580f2807164218
yong <xxy668@foxmail.com>
2016-07-05 19:13:09 +0800

    SOF OOM init

---
+++ out-of-memory/src/main/java/com/henryxi/jvm/JavaVMStackOOM.java
@@ -0,0 +1,29 @@
+package com.henryxi.jvm;
+
+/**
+ * -Xss2M
+ */
+public class JavaVMStackOOM {
+    private void neverStop() {
+        while (true) {
+
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    neverStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
